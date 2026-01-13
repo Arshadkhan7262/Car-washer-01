58996649ea4aba5cec35aea766a55d511bc05e0e
tecramza <veer27420@gmail.com>
2026-01-13 12:37:35 +0500

    adding all folder

--- car_wash_app/ios/Runner/AppDelegate.swift
+++ car_wash_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,10 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Initialize Google Maps
-    // Make sure Maps SDK for iOS is enabled in Google Cloud Console
-    GMSServices.provideAPIKey("AIzaSyDQTjH85etAVOY56-3AZ3oydpI3414ZsMU")
-    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
